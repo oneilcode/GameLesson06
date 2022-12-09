6912617f11f8128e1cebe39ad10faf8050600c62
Viktoriia Oneil <oneilcode111@gmail.com>
2022-12-09 23:40:39 +1000

    Правка кода

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 'use strict';
 
 let question = +prompt('Угадай число от 1 до 100');
+let count = 10;
 
 function game() {
-   let count = 10;
+
    let number = 20;
 
    if (count == 1) {
       confirm('Попытки закончились, хотите сыграть еще?');
       if (question == null) {
          question = alert('Игра окончена');
-         console.log('Игра окончена');
          return;
       } else {
          count = 11;
@@ -33,9 +33,16 @@ function game() {
    } else if (question < number) {
       question = +prompt('Загаданное число больше, осталось попыток ' + count + ' Попробуйте еще раз');
       game();
-   } else if (question === number) {
-      question = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?'); //ВОТ ТУТ ЧТО-ТО НЕ ТАК ПОМОГИТЕ =(
-      game();
+   } else if (question === number) { //вот тут что-то не так ПОМОГИТЕ =(
+      question = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?');
+      if (question == null) {
+         question = alert('Игра окончена');
+         console.log('Игра окончена');
+      } else {
+         count = 11;
+         game();
+      }
+      return question;
    } else if (question !== isNaN) {
       question = +prompt('Введи число!');
       game();
